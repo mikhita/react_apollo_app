87fb474910fb2340d277b7cee9c436ac7bd45e8b
mikhita <mikheilispiriani@yahoo.com>
2023-09-23 17:59:18 +0400

    clonning the project task from github

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import './App.css'
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
 
-function App() {
-  
+const App = () => {
+  const [page, setPage] = useState('authors')
 
   return (
-    <>
+    <div>
       <div>
-        
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
       </div>
-      <h1>Vite + React</h1>
 
-    </>
+      <Authors show={page === 'authors'} />
+
+      <Books show={page === 'books'} />
+
+      <NewBook show={page === 'add'} />
+    </div>
   )
 }
 
